4f870c3f29d04609e2426eed9ba90585a721a3c4
Carlos Serrano <calsemo2001@gmail.com>
2024-05-01 19:30:33 -0400

    first commit

---
+++ Persona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrucoTrato
+{
+    internal class Persona
+    {
+        int age;
+        string name;
+        int height;
+        string type;
+
+        public Persona(int age, string name, int height , string type)
+        {
+            this.age = age;
+            this.name = name;
+            this.height = height;
+            this.type = type.ToUpper();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace TrucoTrato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona[] personas =
+            {
+                new Persona(35, "Brais", 177, "trick"),
+                new Persona(22, "Carlos", 175, "trick"),
+                new Persona(5, "Pedro", 80, "treat"),
+                new Persona(10, "Victoria", 150, "treat")
+            };
+
+
+            Halloween(personas);
+
+        }
+
+
+
+
+        static void Halloween(Persona[] persona)
+        {
+           
+        } 
+
+
+    }
+}
